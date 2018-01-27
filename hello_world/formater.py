e00e41db71af746fb5a5a0aec99bd5497f3542a3
Brokulli <respondek.patrycja@gmail.com>
2018-01-27 14:23:05 +0100

    aktualizacja

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -22,8 +22,6 @@ def get_formatted(msg, imie, format):
 
 def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "mgs":"' +  msg + '"}')
-# wersja z bugiem
-# return ('{ "imie":"' + imie + '", "mgs":' +   msg + '"}')
 
 
 def plain_text(msg, imie):
